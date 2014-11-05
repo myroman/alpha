9f8774e39e52eef3ce9e54ba50179bfb94b79635
Soumadip <stmukherjee@gmail.com>
2014-11-05 17:50:09 -0500

    Added the debug macro so we dont need to use printf all the time. Compile with -DDEBUG to get the debug messages

--- test.c
+++ test.c
@@ -1,5 +1,16 @@
+#ifdef DEBUG
+#define debug(fmt, ...) printf("DEBUG: %s:%s:%d \n" fmt, __FILE__,__FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define debug(fmt, ...)
+#endif
+
+
 #include <stdio.h>
+
+
 int main () {
 	printf("Hello, world!");
-	exit(0);
+	int i =100;
+	debug(i);
+	//exit(0);
 }
