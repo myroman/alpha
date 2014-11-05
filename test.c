164c80e4282243738bcbe99aa8b034db387a550a
Soumadip <stmukherjee@gmail.com>
2014-11-05 18:37:05 -0500

    Fixed the debug and info messages. Make sure to set the -DDEBUG Flag in order for the message to appear

--- test.c
+++ test.c
@@ -1,16 +1,20 @@
 #ifdef DEBUG
-#define debug(fmt, ...) printf("DEBUG: %s:%s:%d \n" fmt, __FILE__,__FUNCTION__, __LINE__, ##__VA_ARGS__)
+	#define debug(fmt, ...) do{printf("DEBUG: %s:%s:%d " fmt, __FILE__,__FUNCTION__, __LINE__, ##__VA_ARGS__); printf("\n");}while(0)
+	#define info(fmt, ...) do{printf("INFO: %s:%s:%d " fmt, __FILE__,__FUNCTION__, __LINE__, ##__VA_ARGS__);printf("\n");}while(0)
 #else
-#define debug(fmt, ...)
+	#define debug(fmt, ...)
+	#define info(fmt, ...) //do{printf("INFO: \n" fmt,##__VA_ARGS__); printf("\n");}while(0)
 #endif
+
 
 
 #include <stdio.h>
 
 
 int main () {
-	printf("Hello, world!");
+	printf("Hello, world!\n");
 	int i =100;
-	debug(i);
+	debug("%d", i);
+	info("Tesing info msg");
 	//exit(0);
 }
