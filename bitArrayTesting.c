f4726d325d6409df06909c6b5f867f1c34327d13
Soumadip <stmukherjee@gmail.com>
2014-11-19 18:26:12 -0500

    bit array working properly

--- bitArrayTesting.c
+++ bitArrayTesting.c
@@ -2,25 +2,138 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <arpa/inet.h>
-#include <strings.h>
+//#include <strings.h>
+#include "debug.h"
+#include <string.h>
 
+typedef struct PayloadHdr PayloadHdr;
+struct PayloadHdr {
+	uint32_t msgType;
+	uint32_t forceRediscovery;
+	uint32_t rrepSent;
+	uint32_t hopCount;
+	uint32_t broadcastId;
+
+	in_addr_t srcIp;
+	in_addr_t destIp;
+
+	uint32_t srcPort;
+	uint32_t  destPort;
+};
+
+void printPayloadContents(PayloadHdr *p){
+	struct in_addr srcIpIa;
+	srcIpIa.s_addr = p->srcIp;
+	struct in_addr destIpIa;
+	destIpIa.s_addr = p->destIp;
+	char destIP [20];
+	char* destIPAddr = inet_ntoa(destIpIa);
+	printf("Source IP is %s\n",inet_ntoa(srcIpIa));
+	printf("Destination IP is %s\n",inet_ntoa(destIpIa));
+	printf("MsgType: %u, forceRediscovery %u, RREP Sent: %u, HopCount: %u, broadcastId %u, source Port: %u, destPort: %u\n",
+	 p->msgType, p->forceRediscovery, p->rrepSent, p->hopCount, p->broadcastId, p->srcPort, p->destPort);
+
+}
+
+
+void insertType(uint32_t type, void* buf);
+void insertFD(uint32_t fd, void* buf);
+void insertRREQ(uint32_t r, void* buf);
+void insertHopCount(uint32_t hopCount , void* buf);
+void insertBroadCastID(uint32_t bID , void* buf);
+void insertSrcPort(uint32_t sPort, void* buf);
+void insertDestPort(uint32_t dPort, void* buf);
+void insertSrcIp(in_addr_t ipAddr, void* buf);
 in_addr_t extractSrcIp(void* buf);
+void insertDestIp(in_addr_t ipAddr, void* buf);
 in_addr_t extractDestIp(void* buf);
+uint32_t extractSrcPort(void *buf);
+uint32_t extractDestPort(void *buf);
+uint32_t extractType(void* buf);
+uint32_t extractFD(void* buf);
+uint32_t extractRREQ(void* buf);
+uint32_t extractHopCount(void* buf);
+uint32_t extractBroadCast(void* buf);
+
+
+void* createNewMsg(PayloadHdr* p ){
+	void * msg_ptr = malloc(17);
+	insertType(p->msgType, msg_ptr);
+	insertFD(p->forceRediscovery, msg_ptr);
+	insertRREQ(p->rrepSent, msg_ptr);
+	insertHopCount(p->hopCount, msg_ptr);
+	insertBroadCastID(p->broadcastId, msg_ptr);
+	
+	insertSrcIp(p->srcIp, msg_ptr);
+	insertDestIp(p->destIp, msg_ptr);
+
+	insertSrcPort(p->srcPort, msg_ptr);
+	insertDestPort(p->destPort, msg_ptr);
+	return msg_ptr;
+}
+
+PayloadHdr * extractMsgContents(void * buf){
+	PayloadHdr *phdr = (PayloadHdr *) malloc(sizeof(PayloadHdr)); 
+	
+	phdr->msgType = extractType(buf);
+	phdr->forceRediscovery = extractFD(buf);
+	phdr->rrepSent = extractRREQ(buf);
+	phdr->hopCount = extractHopCount(buf);
+	phdr->broadcastId = extractBroadCast(buf);
+
+	phdr->srcPort = extractSrcPort(buf);
+	phdr->destPort = extractDestPort(buf);
+
+	phdr->srcIp = extractSrcIp(buf);
+	phdr->destIp = extractDestIp(buf);
+
+	return phdr;
+}
+
+
+
 int main (){
-	void* buf = malloc(17);
+	void * msg_ptr = malloc(17);
+	//insertType(2,msg_ptr );
+	
+	//PayloadHdr* p = (PayloadHdr*) malloc(17);
+	//p->msgType = 2;
+	//insertType(p->msgType, msg_ptr);
+	//extractType(msg_ptr);
+
+	PayloadHdr* p = (PayloadHdr*) malloc(sizeof(PayloadHdr));
+	p->msgType = 2;
+	p->forceRediscovery = 1;
+	p->rrepSent = 0;
+	p->hopCount = 10;
+	p->broadcastId = 20;
+	p->srcPort = 50001;
+	p->destPort = 12456;
+	p->srcIp = inet_addr("127.0.0.1");
+	p->destIp = inet_addr("255.4.2.1");
+
+	void * msgPayload = createNewMsg(p);
+	debug("Here");	
+	debug("Asdf");
+
+	PayloadHdr* extractPhdr = extractMsgContents(msgPayload);
+	printPayloadContents(extractPhdr);
+
+
+
+	/*void* buf = malloc(17);
 	void* firstPart = buf;
 	//void* ports = buf + 12;
 	//first 4 bytes
 	uint32_t result;
 	insertType(2, buf);
-	insertFD(0, buf);
+	insertFD(1, buf);
 	insertRREQ(1, buf);
 	insertHopCount(10, buf);
 	insertBroadCastID(20, buf);
-	*((uint32_t*)firstPart) = result;
+	((uint32_t*)firstPart) = result;
 	void* testPart1 = malloc(4);
 	printf("What I put into buf is %u. The value before is %u\n", (uint32_t)*((uint32_t*)testPart1), result);
-	
 	extractType(buf);
 	extractFD(buf);
 	extractRREQ(buf);
@@ -47,32 +160,24 @@ int main (){
 	insertDestPort(61234, buf);
 	extractSrcPort(buf);
 	extractDestPort(buf);
-
+    */
 	
 }
 
 void insertType(uint32_t type, void* buf){
-	uint32_t* result = malloc(4);
+	uint32_t * result = (uint32_t *)malloc(4);
+	//uint32_t result;
 	memcpy(result, buf, 4);
-	
 	type = type << 30;
 	printf("type shifted to the left by 30: %u\n", type);
 	*result = *result | type;
 	printf("result = %u, buf=%u\n", *result, *((uint32_t*)buf));
 	memcpy(buf, result, 4);
+	debug("here");
 	free(result);
+	debug("here2");
 }
 
-int extractType(void* buf){
-	uint32_t* ptr = malloc(4);
-	memcpy(ptr, buf, 4);
-	uint32_t result = *ptr;
-	free(ptr);
-	result = result & (0xc0000000);
-	result = result >> 30;
-	printf("the type field is %u\n", result);
-	return result;
-}
 
 void insertFD(uint32_t fd, void* buf){
 	uint32_t* result = malloc(4);
@@ -153,24 +258,36 @@ in_addr_t extractDestIp(void* buf) {
 	return *((in_addr_t*) ptr);
 }
 
-int extractSrcPort(void *buf){
+uint32_t extractSrcPort(void *buf){
 	void* ptr = malloc(2);
 	memcpy(ptr, buf + 12, 2);
 	uint32_t result = *((uint32_t*)ptr);
 	result = result & (0xFFFF);
 	printf("The source port is %u\n", result);
 	free(ptr);
+	return result;
 }
-int extractDestPort(void *buf){
+uint32_t extractDestPort(void *buf){
 	void* ptr = malloc(2);
 	memcpy(ptr, buf + 14, 2);
 	uint32_t result = *((uint32_t*)ptr);
 	result = result & (0xFFFF);
 	printf("The dest port is %u\n", result);
 	free(ptr);
+	return result;
+}
+uint32_t extractType(void* buf){
+	uint32_t* ptr = malloc(4);
+	memcpy(ptr, buf, 4);
+	uint32_t result = *ptr;
+	free(ptr);
+	result = result & (0xc0000000);
+	result = result >> 30;
+	printf("the type field is %u\n", result);
+	return result;
 }
 
-int extractFD(void* buf){
+uint32_t extractFD(void* buf){
 	uint32_t* ptr = malloc(4);
 	memcpy(ptr, buf, 4);
 	uint32_t result = *ptr;
@@ -181,7 +298,7 @@ int extractFD(void* buf){
 	return result;
 }
 
-int extractRREQ(void* buf){
+uint32_t extractRREQ(void* buf){
 	uint32_t* ptr = malloc(4);
 	memcpy(ptr, buf, 4);
 	uint32_t result = *ptr;
@@ -192,7 +309,7 @@ int extractRREQ(void* buf){
 	return result;
 }
 
-int extractHopCount(void* buf){
+uint32_t extractHopCount(void* buf){
 	uint32_t* ptr = malloc(4);
 	memcpy(ptr, buf, 4);
 	uint32_t result = *ptr;
@@ -203,7 +320,7 @@ int extractHopCount(void* buf){
 	return result;
 }
 
-int extractBroadCast(void* buf){
+uint32_t extractBroadCast(void* buf){
 	uint32_t* ptr = malloc(4);
 	memcpy(ptr, buf, 4);
 	uint32_t result = *ptr;
